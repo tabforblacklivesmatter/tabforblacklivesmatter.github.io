f45deb4530c711eac81503a82c5f675d841f265f
Lena Renshaw <lenarenshaw@Lenas-MacBook-Pro-2.local>
2020-06-13 15:48:04 -0400

    added links

---
+++ scripts.js
@@ -0,0 +1,7 @@
+$(function () {
+    $("#download").hover(function (e) {
+        $('#download').html("<h4>Coming soon</h4>");
+    }, function (e) {
+        $('#download').html("<h4>Download extension</h4>");
+    });
+});
